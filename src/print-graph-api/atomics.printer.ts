cee27771dca192f81218d862fd62b4e5c0d34c8e
b41ex <bochencev.qubership@gmail.com>
2025-09-26 16:39:07 +0300

    fix: remove excessve

--- src/print-graph-api/atomics.printer.ts
+++ src/print-graph-api/atomics.printer.ts
@@ -76,18 +76,10 @@ export function printDescription(value?: string, indent = '', firstInBlock = tru
 }
 
 export function printString(str: string): string {
-  // Escape all control characters according to GraphQL string literal specification
-  // See: https://spec.graphql.org/October2021/#sec-String-Value
-  
-  // Chained replace() calls are actually fastest due to V8 optimizations
+  // Escape only backslashes and quotes
   const escaped = str
     .replace(/\\/g, '\\\\')           // Escape backslashes first (must be first to avoid double-escaping)
     .replace(/"/g, '\\"')             // Escape quotes
-    .replace(/\n/g, '\\n')            // Escape newlines (line feed)
-    .replace(/\r/g, '\\r')            // Escape carriage returns
-    .replace(/\t/g, '\\t')            // Escape tabs (horizontal tab)
-    .replace(/[\b]/g, '\\b')          // Escape backspace (using character class to avoid \b word boundary)
-    .replace(/\f/g, '\\f')            // Escape form feed
   
   return `"${escaped}"`
 }
