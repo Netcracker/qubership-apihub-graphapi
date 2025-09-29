8196ed2b111a323c4c59fb93733287f941d06870
b41ex <b41ex@users.noreply.github.com>
2025-09-29 12:17:17 +0000

    deploy: b2cf9bceea08c3c53999086e6a2fdf33ef238364

--- feature/performance-optimization/assets/syntaxhighlighter-NMPM6SWI-BYjSQSqx.js
+++
@@ -1,2 +0,0 @@
-import{S as s,c,s as l}from"./DocsRenderer-NNNQARDV-CwSOZoP6.js";import"./iframe-YDPJdBex.js";import"../sb-preview/runtime.js";import"./index-CSJjS6Ct.js";import"./_getPrototype-BjV9fylb.js";import"./index-Cu4lwwaE.js";import"./index-ogSvIofg.js";export{s as SyntaxHighlighter,c as createCopyToClipboardFunction,l as default};
-//# sourceMappingURL=syntaxhighlighter-NMPM6SWI-BYjSQSqx.js.map
