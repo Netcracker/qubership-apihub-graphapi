5310aa9be8ca29191f8055338d341d8c65e1d68f
b41ex <b41ex@users.noreply.github.com>
2026-02-05 14:14:36 +0000

    deploy: 0d34af93e09bae20ff3177d3e7015890ca499e06

--- release/assets/preview-CKUXRgLD.js
+++
@@ -1,8 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-B9HWL9GD.js","./iframe-Bry5gMxs.js","./index-CSJjS6Ct.js","./_getPrototype-BjV9fylb.js","./index-Cu4lwwaE.js","./index-ogSvIofg.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as t}from"./iframe-Bry5gMxs.js";import"../sb-preview/runtime.js";var n={docs:{renderer:async()=>{let{DocsRenderer:e}=await t(()=>import("./DocsRenderer-NNNQARDV-B9HWL9GD.js").then(r=>r.D),__vite__mapDeps([0,1,2,3,4,5]),import.meta.url);return new e}}};export{n as parameters};
-//# sourceMappingURL=preview-CKUXRgLD.js.map
